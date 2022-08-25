6db5ec393c3f1b363a900664b9f6b5b8cf15bf22
MentoskaPraha <filipjanstary@gmail.com>
2022-08-25 13:33:38 +0300

    Finished 320px width support

---
+++ scripts/navBar.js
@@ -0,0 +1,13 @@
+//Toggles the menu when screen is too small to fit nav bar
+function toggleNav(){
+    var menu = document.getElementById("menu");
+    var menuButton = document.getElementById("menuButton")
+
+    if (x.style.display === "block") {
+        menu.style.display = "none";
+        menuButton.style.borderRadius = "5px"
+    } else {
+        menu.style.display = "block";
+        menuButton.style.borderRadius = "0px"
+    }
+}
