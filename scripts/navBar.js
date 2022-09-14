77f5a1eeee21d0050460dae95de9612f907b9ddf
MentoskaPraha <filipjanstary@gmail.com>
2022-09-14 11:19:35 +0300

    More work on header nav menu

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,13 +1,16 @@
 //Toggles the menu when screen is too small to fit nav bar
 function toggleNav(){
-    var menu = document.getElementById("menu");
-    var menuButton = document.getElementById("menuButton")
+    const menu = document.getElementById("menu");
+    const menuButton = document.getElementById("menuButton");
+    const banner = document.getElementById("pageBanner");
 
     if (menu.style.display === "block") {
         menu.style.display = "none";
-        menuButton.style.borderRadius = "5px"
+        menuButton.style.borderRadius = "5px";
+        banner.style.marginTop = "50px";
     } else {
         menu.style.display = "block";
-        menuButton.style.borderRadius = "0px"
-    }
+        menuButton.style.borderRadius = "0px";
+        banner.style.marginTop = "100px";
+    }    
 }
