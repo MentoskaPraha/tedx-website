b1fa504606dc239c8f0f58e8de9f7798a2d344bd
MentoskaPraha <filipjanstary@gmail.com>
2022-09-14 12:52:07 +0300

    Added favicon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //libraries
 const express = require('express');
-const path = require('path');
+const favicon = require('serve-favicon');
 
 //get enviroment varriables into place
 require('dotenv').config();
@@ -8,14 +8,12 @@ require('dotenv').config();
 //create and configure app
 const app = express();
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/images/Favicon.ico'));
 
 //create route listen event
 app.get("/", (req, res) => {
     //send file
-    res.sendFile(__dirname + '/public/pages/home.html');
-
-    //log request
-    console.log('User requested the home page.');
+    res.sendFile(__dirname + '/public/pages/home.html')
 });
 
 app.listen(process.env.PORT, () => console.log("App is now online."));
